c31936343e47b6a2eef0b63afa14719bd1d4f871
sandesh <bhujbalsandesh52@gmail.com>
2024-02-15 00:17:27 +0530

    add the icon for the valid checkbox

--- Form Validation/script.js
+++ Form Validation/script.js
@@ -3,3 +3,18 @@ var phoneError = document.getElementById('phone-error');
 var emailError = document.getElementById('email-error');
 var messageError = document.getElementById('message-error');
 var submitError = document.getElementById('submit-error');
+
+function validateName(){
+    var name = document.getElementById('contact-name').value;
+    
+    if(name.length == 0){
+        nameError.innerHTML = 'Name is required';
+        return false;
+    }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = 'Write Full Name';
+        return false;
+    }
+    nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
