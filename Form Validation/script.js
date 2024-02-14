561792dd348855e57d68a4e7960c8dcfbc9843dc
sandesh <bhujbalsandesh52@gmail.com>
2024-02-14 23:31:14 +0530

    add script.js file create variable for input group

---
+++ Form Validation/script.js
@@ -0,0 +1,5 @@
+var nameError = document.getElementById('name-error');
+var phoneError = document.getElementById('phone-error');
+var emailError = document.getElementById('email-error');
+var messageError = document.getElementById('message-error');
+var submitError = document.getElementById('submit-error');
